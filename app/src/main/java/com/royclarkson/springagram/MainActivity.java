6fd38729dc90729d0cd31f90ed05aae5068ab504
Roy Clarkson <rclarkson@pivotal.io>
2014-09-04 09:53:52 -0500

    Remove unused code

--- app/src/main/java/com/royclarkson/springagram/MainActivity.java
+++ app/src/main/java/com/royclarkson/springagram/MainActivity.java
@@ -23,11 +23,8 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.royclarkson.springagram.model.ApiResource;
 import com.royclarkson.springagram.model.GalleryResource;
@@ -199,19 +196,6 @@ public class MainActivity extends Activity
 				.commit();
 	}
 
-	public void onSectionAttached(int number) {
-		switch (number) {
-			case 1:
-				title = getString(R.string.title_section_home);
-				break;
-			case 2:
-				title = getString(R.string.title_section_photos);
-				break;
-			case 3:
-				title = getString(R.string.title_section_galleries);
-				break;
-		}
-	}
 
 
 	//***************************************
@@ -377,47 +361,6 @@ public class MainActivity extends Activity
 	@Override
 	public void onRemovePhotoFromGalleryByPosition(int position) {
 		this.photos.remove(position);
-	}
-
-
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-		/**
-		 * The fragment argument representing the section number for this
-		 * fragment.
-		 */
-		private static final String ARG_SECTION_NUMBER = "section_number";
-
-		/**
-		 * Returns a new instance of this fragment for the given section
-		 * number.
-		 */
-		public static PlaceholderFragment newInstance(int sectionNumber) {
-			PlaceholderFragment fragment = new PlaceholderFragment();
-			Bundle args = new Bundle();
-			args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-			fragment.setArguments(args);
-			return fragment;
-		}
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-								 Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-			return rootView;
-		}
-
-		@Override
-		public void onAttach(Activity activity) {
-			super.onAttach(activity);
-			((MainActivity) activity).onSectionAttached(
-					getArguments().getInt(ARG_SECTION_NUMBER));
-		}
 	}
 
 }
